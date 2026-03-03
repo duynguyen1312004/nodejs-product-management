8098325c60d3f828f9a35a76699ac18dc23d075f
duynguyen1312004 <nguyenduyk2002@gmail.com>
2026-03-03 12:55:47 +0700

    2. tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -25,7 +25,7 @@ module.exports.index = async (req, res) => {
 
     const objectSearch = searchHelper(req.query);
 
-    console.log(objectSearch);
+    // console.log(objectSearch);
 
     if(objectSearch.regex)
     {
@@ -33,14 +33,40 @@ module.exports.index = async (req, res) => {
         find.title = objectSearch.regex;
     }
 
-    const products = await Product.find(find);
+    //Pagination
 
-    // console.log(products);
+    let objectPagination = {
+        currentPage: 1,
+        limitItem: 4
+    };
+
+    if(req.query.page){
+        objectPagination.currentPage = parseInt(req.query.page) ;
+    }
+
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+    const countProducts = await Product.countDocuments(find);
+
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+    
+
+    objectPagination.totalPage = totalPage;
+
+
+
+
+    //End Pagination
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip
+    (objectPagination.skip);
+
 
     res.render("admin/pages/products/index",{
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });//?truy cập vào pug
 }
